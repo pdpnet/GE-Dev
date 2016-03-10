e28ae209c07c548a642d1ebaa83637b8e55fc78d
Felipe Hernandez <pdpnet_2007@live.com.mx>
2016-03-10 13:08:05 -0600

    Se actualizo el Index

---
+++ GE-Usuarios/GE-Usuarios/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GE_Usuarios
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ GE-Usuarios/GE-Usuarios/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GE_Usuarios.MvcApplication" Language="C#" %>
